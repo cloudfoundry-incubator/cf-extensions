718260b132bda75f1be49be802a20c3e97ed58cd
dr.max <maximilien@acm.org>
2017-07-19 11:39:07 -0700

    added UT for bot/utilities.go and made funcs testable

---
+++ bot/utilities_test.go
@@ -0,0 +1,76 @@
+package bot
+
+import (
+	"github.com/maximilien/cf-extensions/models"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"time"
+)
+
+var _ = Describe("utilities", func() {
+
+	var infos models.Infos
+
+	BeforeEach(func() {
+		infos = models.Infos{
+			models.Info{
+				Name:        "info0",
+				Description: "Info0 description",
+				GitUrl:      "info0.git.url",
+				TrackerUrl:  "info0.tracker.url",
+
+				OwnerCompany: "info0 Inc.",
+				ContactEmail: "info@info0.com",
+				ProposedDate: "",
+
+				Repo: nil,
+			},
+			models.Info{
+				Name:        "info1",
+				Description: "Info1 description",
+				GitUrl:      "info1.git.url",
+				TrackerUrl:  "info1.tracker.url",
+
+				OwnerCompany: "info1 Inc.",
+				ContactEmail: "info@info1.com",
+				ProposedDate: "",
+
+				Repo: nil,
+			},
+		}
+	})
+
+	Context("#Length", func() {
+		It("returns the length of the Infos", func() {
+			Expect(Length(infos)).To(Equal(2))
+			Expect(Length(infos)).To(Equal(infos.Len()))
+		})
+	})
+
+	Context("#CurrentTime", func() {
+		It("return current time", func() {
+			Expect(CurrentTime()).ToNot(Equal(time.Now()))
+		})
+	})
+
+	Context("#FormatAsDate", func() {
+		It("formats time.Time as date string", func() {
+			currentTime := time.Date(2017, 7, 19, 0, 0, 0, 0, time.Local)
+			Expect(FormatAsDate(currentTime)).To(Equal("7/19/2017"))
+		})
+	})
+
+	Context("#FormatAsDateTime", func() {
+		It("formats time.Time as date and time string", func() {
+			currentTime := time.Date(2017, 7, 19, 11, 30, 0, 0, time.Local)
+			Expect(FormatAsDateTime(currentTime)).To(Equal("7/19/2017 @ 11:30:0"))
+		})
+	})
+
+	Context("#ParseAsDate", func() {
+		It("parses date and date/time string as a time.Time", func() {
+			//Fail("Implement me!")
+		})
+	})
+})
